33cd023a3425cc750b387f3159400b6a57594d63
linkchiu <linkchiu007@gmail.com>
2015-03-09 12:53:29 +1100

    x

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,14 @@
+require "net/http"
+require "uri"
 class DonationsController < ApplicationController
   def new
+  	uri = URI.parse("http://google.com/")
+
+# Shortcut
+response = Net::HTTP.get_response(uri)
+
+# Will print response.body
+Net::HTTP.get_print(uri)
   	@donation = Donation.new
   end
 
@@ -7,7 +16,7 @@ class DonationsController < ApplicationController
   	# @donation = Donation.new(address:params[:address])
   	# @donation.save
   	# puts params[:card_token]
-  	
+
   end
 
   def show
