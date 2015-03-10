70f833145048b180cbfc8a949fd071d268049521
linkchiu <linkchiu007@gmail.com>
2015-03-10 22:48:10 +1100

    implement invoke charge api

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,20 +1,28 @@
 require "net/http"
-require "uri"
 class DonationsController < ApplicationController
   def new
-  	uri = URI.parse("http://google.com/")
 
-# Shortcut
-response = Net::HTTP.get_response(uri)
-
-# Will print response.body
-Net::HTTP.get_print(uri)
-  	@donation = Donation.new
   end
 
   def create
-  	# @donation = Donation.new(address:params[:address])
-  	# @donation.save
+  	@token = params[:card_token]
+	uri = URI.parse('https://test-api.pin.net.au/1/charges')
+	res = Net::HTTP.start(uri.host, uri.port,:use_ssl => uri.scheme == 'https') do |http|
+		req = Net::HTTP::Post.new(uri.path)
+		req.basic_auth '6gf2Czf2xc4VoNpy3waq1Q', ' '
+		req.set_form_data({'email' => 'linkchiu007@gmail.com','amount' => '800', 
+			'currency' => "USD",'description' => 'for test charge','ip_address' => '118.139.3.129','card_token' => @token})
+		http.request(req)
+	end
+	case res
+	when Net::HTTPSuccess, Net::HTTPRedirection
+  		@message = res.value
+	else
+		render 'show'
+  	 @message = res.value
+	end
+  	 # @donation = Donation.new(address:params[:address])
+  	 # @donation.save
   	# puts params[:card_token]
 
   end
