304760201a5d1d2047abdf9ee936552e32331c66
linkchiu <linkchiu007@gmail.com>
2015-04-02 19:34:50 +1100

    add production config for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    port:587,
+    authentication:"plain",
+    enable_starttls_auto:true,
+    domain: "gmail.com",
+    user_name:"linkchiu007@gmail.com",
+    password:"QLJ==891024"
+  }
+
+
 end
